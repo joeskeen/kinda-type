4a4d9044ffe8a35bf6da4253b0939bcf6d81bae8
Joe Skeen <joeskeen@outlook.com>
2023-01-31 09:57:35 -0700

    fix error

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 // based on https://stackoverflow.com/a/64060332/1396477
 declare type Kinda<T> = {
   [P in keyof T]?: T[P] extends Array<infer U> ? Array<Value<U>> : Value<T[P]>;
-} & T;
+};
 type AllowedPrimitives =
   | boolean
   | string
